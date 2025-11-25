a22c7a590ba2aea051d59f69fd766a79d6428d0c
cwm <carloswashingtonmercado@gmail.com>
2025-11-25 12:58:27 -0300

    update # annotations separated from Student to StudentDto, MVC and EF separation

--- templates/BasicNtierTemplate/BasicNtierTemplate.Service/Mappings/StudentProfile.cs
+++ templates/BasicNtierTemplate/BasicNtierTemplate.Service/Mappings/StudentProfile.cs
@@ -13,10 +13,19 @@ namespace BasicNtierTemplate.Service.Mappings
             //    .ForMember(dto => dto.some_field, opt => opt.MapFrom(src => src.SomeFiled))
             //    ;
 
+            // Student -> StudentDto
             CreateMap<Student, StudentDto>()
-                .ForMember(destination => destination.Enrollments, options => options.Ignore()) // With this line avoid reference loop
-                .ReverseMap()
-                ;
+                .ForMember(destination => destination.FullName, options => options.Ignore())
+                .ForMember(dest => dest.GovernmentIdFormatted, opt => opt.Ignore())
+                .ForMember(dest => dest.Enrollments, opt => opt.Ignore())
+            ;
+
+            // StudentDto -> Student (explicit control)
+            CreateMap<StudentDto, Student>()
+                .ForMember(destination => destination.FullName, options => options.Ignore())
+                .ForMember(dest => dest.Enrollments, opt => opt.Ignore()) // Handle separately
+            ;
+
         }
     }
 }
