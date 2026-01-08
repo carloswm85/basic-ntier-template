ec0e337c008d68431c9991f5a13241bf93cdeef7
Carlos W. Mercado (laptop) <carloswashingtonmercado@gmail.com>
2026-01-08 12:24:51 -0300

    deleted # unused files, keeping only documents and branch references

--- templates/BasicNtierTemplate/BasicNtierTemplate.Data/Entities/Partials/Posteo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BasicNtierTemplate.Data.Model
-{
-    public partial class Posteo : IEntity
-    {
-        public object ID => id;
-    }
-}
--- templates/BasicNtierTemplate/BasicNtierTemplate.Repository/Repository/IStoredProcedureRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BasicNtierTemplate.Repository
-{
-    public interface IStoredProcedureRepository<T> where T : class
-    {
-        IEnumerable<T> Exec(Dictionary<string, object> parameters);
-    }
-}
