04fa6eea5764d2c4364bf8aa5be6e44f61e81d87
Carlos W. Mercado (laptop) <carloswashingtonmercado@gmail.com>
2026-02-04 10:35:20 -0300

    update # API and SPA launch files and configuration

--- templates/BasicNtierTemplate/src/BasicNtierTemplate.Web.Angular/src/proxy.conf.js
+++ templates/BasicNtierTemplate/src/BasicNtierTemplate.Web.Angular/src/proxy.conf.js
@@ -1,22 +1,25 @@
-const { env } = require('process');
+const { env } = require("process");
 
 const target = env.ASPNETCORE_HTTPS_PORT
     ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
     : env.ASPNETCORE_URLS
-        ? env.ASPNETCORE_URLS.split(';')[0]
-        : 'http://localhost:5001';
-
+      ? env.ASPNETCORE_URLS.split(";")[0]
+      : "http://localhost:5001";
 
 const PROXY_CONFIG = [
-  {
-    context: [
-      "/api/WeatherForecasts/data",
-    ],
-    target,
-    secure: false
-  }
-]
+    {
+        context: ["/api/WeatherForecasts/data"],
+        proxyTimeout: 10000,
+        target: target,
+        secure: false,
+        headers: {
+            Connection: "Keep-Alive",
+        },
+    },
+];
 
+//console.log(`\nenv.ASPNETCORE_HTTPS_PORT: ${env.ASPNETCORE_HTTPS_PORT}\n`);
+//console.log(`\nenv.ASPNETCORE_URLS: ${env.ASPNETCORE_URLS}\n\n`);
 //console.log(`\nCurrent target: ${target}\n`);
 //console.log("\nCurrent PROXY_CONFIG:\n", PROXY_CONFIG, "\n");
 
