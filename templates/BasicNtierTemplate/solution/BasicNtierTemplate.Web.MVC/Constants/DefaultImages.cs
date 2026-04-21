80edd571a5fd2d4ba128c58b48b3dcb02ec66325
Carlos W. Mercado (laptop) <carloswashingtonmercado@gmail.com>
2026-04-21 14:15:26 -0300

    update # default-students to default-person

--- templates/BasicNtierTemplate/solution/BasicNtierTemplate.Web.MVC/Constants/DefaultImages.cs
+++ templates/BasicNtierTemplate/solution/BasicNtierTemplate.Web.MVC/Constants/DefaultImages.cs
@@ -2,5 +2,5 @@
 
 public static class DefaultImages
 {
-    public const string Student = "/images/defaults/default-students.png";
+    public const string Student = "/images/defaults/default-person.png";
 }
